255286076a458d99d381d397ebd5599eb59f18a3
Inserer-Pseudo <ben10.cob@gmail.com>
2023-11-22 09:24:39 +0100

    the end

--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -5,5 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		EditorGui gui1 = new EditorGui();
 		EditorGui gui2 = new EditorGui();
+		EditorGui gui3 = new EditorGui();
+		EditorGui gui4 = new EditorGui();
 	}
 }
